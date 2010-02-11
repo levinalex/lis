d85b2bf6476ee75d0bf27a57dc1064fe689a2238
Levin Alexander <mail@levinalex.net>
2010-02-12 00:08:46 +0100

    untested implementation of send_pendeing_requests

--- lib/lis/application_protocol.rb
+++ lib/lis/application_protocol.rb
@@ -12,7 +12,7 @@ module LIS::Transfer
     end
 
     def received_header(message)
-      @patient_information ||= {} # delete the list of patients
+      @patient_information_requests ||= {} # delete the list of patients
       @device_name = message.sender_name
     end
 
@@ -34,9 +34,30 @@ module LIS::Transfer
     end
 
     def received_request_for_information(message)
-      @patient_information ||= {}
-      requests = @on_request_callback.call(p.starting_range)
-      @patient_information[p.sequence_number] = requests if requests
+      @patient_information_requests ||= {}
+      requests = @on_request_callback.call(@device_name, message.starting_range_id)
+      @patient_information_requests[message.sequence_number] = requests if requests
+    end
+
+    def send_pending_requests
+      return if @patient_information_requests.nil?
+
+      write :begin
+      @patient_information_requests.each do |sequence_nr, data|
+        p "sequence: #{sequence_nr.inspect}"
+        p "data: #{data.inspect}"
+
+        write :message, LIS::Message::Patient.new(sequence_nr,
+                                                  data["patient"]["number"],
+                                                  data["patient"]["last_name"],
+                                                  data["patient"]["first_name"])
+        data["types"].each do |request|
+          write :message, LIS::Message::Order.new(sequence_nr, data["id"], request)
+        end
+      end
+      @patient_information_requests = nil
+
+      write :idle
     end
 
     def initialize(*args)
@@ -59,6 +80,7 @@ module LIS::Transfer
           @last_patient = nil
           @last_order = nil
         when :idle
+          send_pending_requests
         when :message
           @message = LIS::Message::Base.from_string(message)
           handler = @handlers[@message.class]
@@ -66,5 +88,4 @@ module LIS::Transfer
       end
     end
   end
-
 end
