7d555b2cf9f61a20e08d218e1671a14968c0b17b
Levin Alexander <mail@levinalex.net>
2010-02-13 20:33:45 +0100

    documentation improvements

--- lib/lis/io_listener.rb
+++ lib/lis/io_listener.rb
@@ -2,41 +2,73 @@ require 'strscan'
 
 module LIS::Transfer
 
-  # a chainable IO-Listener that provides to methods:
+  # a chainable IO-Listener that provides two methods:
   #
-  # +on_data+ :: a callback that is called whenever a message is received
-  # +write+   :: can be called so send messages to the underlying IO
+  # {#on_data} ::
+  #   a callback that is called whenever a message is received
+  # {#write} ::
+  #   can be called so send messages to the underlying IO
   #
   # when overriding this class, you need to implement two methods:
   #
-  # +receive+ :: is called from an underlying IO whenever a message is received
-  #              the message can be handled, if messages should be propagated, you
-  #              need to call `forward`
+  # {#receive} ::
+  #   is called from an underlying IO whenever a message is received
+  #   the message can be handled.  Call {#forward} to propagate
   #
-  # +write+ :: when data needs to be encoded, formatted before it can be send
-  #            send it with `super`
+  # {#write} ::
+  #   transform/encode a message before sending it. Call +super+ to propagate
   #
+  # See {LineBasedProtocol} for a toy implementation which strips
+  # newlines and only forwards complete lines.
   #
   class Base
+
+    # @param [Base, #on_data] read data source
+    # @param [Base, IO, #write] write object where messages are written to
+    #
     def initialize(read, write = read)
       @reader, @writer = read, write
       @on_data = nil
       @reader.on_data { |*data| receive(*data) } if @reader.respond_to?(:on_data)
     end
 
+    # register a block, to be run whenever the protocol implementation
+    # receives data (by calling {#forward})
+    #
+    # this is used to chain protocol layers together
+    #
+    # @return [self]
+    # @yield [*args] called whenever the protocol implementaion calls {#forward}
+    # @yieldreturn [nil]
+    #
     def on_data(&block)
       @on_data = block
+      self
     end
 
-    def write(*args)
-      @writer.<<(*args) if @writer
-    end
+    # @see #write
     def <<(*args)
       write(*args)
     end
 
-    private
 
+    # write data to underlying interface. override if data needs to be preprocessed
+    #
+    def write(*args)
+      @writer.<<(*args) if @writer
+    end
+
+
+    protected
+
+    # override this method to handle data received from an underlying interface, for
+    # example splitting it into messages or only passing on complete lines
+    # (see {LineBasedProtocol#receive} for an example)
+    #
+    # call {#forward} to pass it on to higher levels
+    #
+    # @return [nil]
+    #
     def receive(data)
       forward(data)
     end
@@ -48,6 +80,11 @@ module LIS::Transfer
 
 
   class LineBasedProtocol < Base
+
+    # strip newlines from received data and pass on complete lines
+    #
+    # @param [String] data
+    #
     def receive(data)
       @memo ||= ""
       scanner = StringScanner.new(@memo + data)
@@ -58,6 +95,9 @@ module LIS::Transfer
       nil
     end
 
+    # add a newline to data and pass it on
+    # @param [String] data
+    #
     def write(data)
       super(data + "\n")
     end
