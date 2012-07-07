2e5e1f968395aa7887bc8f7c455647681f5cd1cb
Levin Alexander <mail@levinalex.net>
2012-07-07 17:06:04 +0200

    use Webmock in unit tests

--- test/helper.rb
+++ test/helper.rb
@@ -1,11 +1,14 @@
 require 'rubygems'
 require 'test/unit'
+require 'webmock/test_unit'
 require 'shoulda'
 require 'mocha'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'lis'
+
+WebMock.disable_net_connect!
 
 class Test::Unit::TestCase
 end
