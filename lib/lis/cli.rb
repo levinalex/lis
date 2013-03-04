d90032e0b71e5cde2fca51e58474270cf792f817
Levin Alexander <mail@levinalex.net>
2013-03-04 13:42:18 +0100

    no reason to drop 1.8 compat

--- lib/lis/cli.rb
+++ lib/lis/cli.rb
@@ -4,13 +4,13 @@ module LIS
     program_desc "LIS interface to Siemens Immulite 2000XPi or other similar analyzers"
     version LIS::VERSION
 
-    flag ["config","c"], default_value: File.join(ENV['HOME'],'.lis'),
-                   desc: "stores configuration and authentication data",
-                   arg_name: "FILE"
+    flag ["config","c"], :default_value => File.join(ENV['HOME'],'.lis'),
+                   :desc => "stores configuration and authentication data",
+                   :arg_name => "FILE"
 
-    flag ["listen", "l"], arg_name: "PORT", desc: "which port to listen on", default_value: "/dev/ttyUSB0"
-    flag ["endpoint", "e"], arg_name: "URI", desc: "HTTP endpoint", default_value: "http://localhost/api/lis"
-    switch ["verbose", "v"], desc: "Increase verbosity"
+    flag ["listen", "l"], :arg_name => "PORT", :desc => "which port to listen on", :default_value => "/dev/ttyUSB0"
+    flag ["endpoint", "e"], :arg_name => "URI", :desc => "HTTP endpoint", :default_value => "http://localhost/api/lis"
+    switch ["verbose", "v"], :desc => "Increase verbosity"
 
     pre do |global_options,command,options,args|
       $VERBOSE = global_options[:verbose]
